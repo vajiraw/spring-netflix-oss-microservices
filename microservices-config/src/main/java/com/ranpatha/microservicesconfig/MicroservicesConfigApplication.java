2995c371720b3a120d94828e90a4eaf559dd4b41
Vajira <vajirawke@gmail.com>
2018-09-07 14:38:03 +0530

    initail commit

---
+++ microservices-config/src/main/java/com/ranpatha/microservicesconfig/MicroservicesConfigApplication.java
@@ -0,0 +1,12 @@
+package com.ranpatha.microservicesconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesConfigApplication.class, args);
+	}
+}
---
+++ microservices-config/src/test/java/com/ranpatha/microservicesconfig/MicroservicesConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ranpatha.microservicesconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicesConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
